a898046035c67470e22c888d9ca50ebc5ca62694
Casper Kangas <casperkangas@gmail.com>
2023-09-25 18:20:53 +0300

    starting code

---
+++ stensaxpase.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+sten = 1
+sax = 2
+pase = 3
+
+print("********************")
+time.sleep(0.5)
+print("Sten - Sax - Påse")
+time.sleep(0.5)
+print("********************")
+
