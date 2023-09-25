7033539238e8636858bfeb612a13e4672921f846
Casper Kangas <casperkangas@gmail.com>
2023-09-25 18:27:26 +0300

    forsok and variables

--- stensaxpase.py
+++ stensaxpase.py
@@ -1,13 +1,19 @@
 import random
 import time
 
-sten = 1
-sax = 2
-pase = 3
+sten = 0
+sax = 0
+pase = 0
+minaPoang = 0
+datornPoang = 0
+forsok = 0
 
 print("********************")
 time.sleep(0.5)
 print("Sten - Sax - Påse")
 time.sleep(0.5)
 print("********************")
+time.sleep(0.5)
+print("")
 
+forsok = int(input("Hur många poäng behövs för att vinna? "))
